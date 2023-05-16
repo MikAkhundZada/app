e71f758ee627c728dea9bc58a05f3a520aa9fc96
Mika <miguelittoakhundo@gmail.com>
2023-05-16 22:46:10 +0300

    changelog

--- js/main.js
+++ js/main.js
@@ -11,11 +11,6 @@ $(function(){
     let clearForm = function(){
         $('.popup-container > input[type="text"]').val('');
     };
-
-    $('.order-call').on('click', function(){
-        showPopup();
-    });
-
     $('.close').on('click', function(){
         clearForm();
         closePopup();
